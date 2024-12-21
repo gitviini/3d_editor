190f993a98e2ca63415ca1068930800ef6c98901
gitviini <gvinicius105@gmail.com>
2024-12-21 02:12:26 -0300

    "new host database"

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost:3306';
-$user = 'root';
-$pass = '';
-$db = 'game';
+$host = 'dpg-ctj4f8rqf0us739961hg-a.ohio-postgres.render.com/game_94iz';
+$user = 'admin';
+$pass = 'kS25vP0A6zv1NxuDHxqixpCK8SmgVwea';
+$db = 'game_94iz';
 
 $con = mysqli_connect($host,$user,$pass,$db);
 ?>
